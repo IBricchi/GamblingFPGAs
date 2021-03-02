f06caf9d420154ed60c6d2a8297f27e41fe1d7da
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2021-03-02 19:52:06 +0000

    Add basic server setup script

---
+++ server/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script for installing all necessary dependencies
+# Make sure to reboot the machine after running this script to apply the changes to /etc/profile
+
+# Install Golang
+sudo apt-get update
+sudo apt-get -y upgrade
+
+wget https://dl.google.com/go/go1.16.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
+
+echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
