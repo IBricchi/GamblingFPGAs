d6b9a207cce84797e8d67bccc1a5bbdf9a7d923d
IBricchi <ignacio.bricchi@gmail.com>
2021-03-04 23:09:06 +0000

    Add p5.js download dependencies in setup

--- web_server/setup.sh
+++ web_server/setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# update
 apt-get update
-apt-get -y install --no-install-recommends python3 python3-pip python3-venv make
+apt-get -y install --no-install-recommends python3 python3-pip python3-venv curl
+
+# setup python
 python3 -m venv env
 ./env/bin/pip3 install -r requirements.txt
+
+# download js dependencies
+mkdir -p static/p5
+curl -o static/p5/p5.js https://cdn.jsdelivr.net/npm/p5@1.2.0/lib/p5.js
+curl -o static/p5/p5.min.js https://cdn.jsdelivr.net/npm/p5@1.2.0/lib/p5.min.js
+
+# gitignore created setups
+echo "*" > env/.gitignore
 mkdir -p __pycache__
 echo "*" > __pycache__/.gitignore
-echo "*" > env/.gitignore
+echo "*" > static/p5/.gitignore
