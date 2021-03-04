cf288154a9da38b142c445f1a58b1e586cfc1c55
IBricchi <ignacio.bricchi@gmail.com>
2021-03-04 22:13:52 +0000

    Change setup of flask server to load information off setup.json

--- web_server/app.py
+++ web_server/app.py
@@ -6,8 +6,8 @@ import threading
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 data = {}
+setup = {}
 
 # setup web ports
 @app.route('/')
@@ -24,19 +24,24 @@ def curl_data():
     time.sleep(10)
 
     global data
-    headers = {
-        'Accept': 'application/json',
-    }
+    global setup
+    data_url = setup["data_url"]
+    headers = setup["headers"]
+    data = setup["data"]
     
     while True:
-        response = requests.get('https://random-data-api.com/api/beer/random_beer', headers=headers)
+        response = requests.get(data_url, headers=headers, data=data)
         data = json.loads(response.content.decode('utf-8'))
         time.sleep(1)
     
 if __name__ == '__main__':
+    # get setup data
+    with open("setup.json", "r") as f:
+        setup = json.loads(f.read())
+
     # run trhead
     t = threading.Thread(target=curl_data, daemon=True)
     t.start()
 
     # run flask
-    app.run()
+    app.run(port = setup["port"])
--- web_server/readme.md
+++ web_server/readme.md
@@ -16,13 +16,14 @@ To run the server simply run.
   ./run.sh
 ```
 
+Confugurations for the server can be changed in setup.json file. This file contains setup data for port the servers should listen on, what headers to use in curl request, data to use in curl request, and what url to use for curl requrest
+
 ### Web Files
 
 Main web page is hosted on:
-http://127.0.0.1:5000/
+Currently only displays json back to user
+http://127.0.0.1:port/
 
 Mirror of json file is hosted on:
-http://127.0.0.1:5000/data/
-
-
+http://127.0.0.1:port/data/
 
