557c6e7fdd281481b8a047c256c45bc86ea03d5b
nicholas-p1 <nicholas.pfaff19@ic.ac.uk>
2021-03-05 15:06:33 +0000

    Add missing dependencies to server setup script

--- server/setup.sh
+++ server/setup.sh
@@ -3,11 +3,14 @@
 # Script for installing all necessary dependencies
 # Make sure to reboot the machine after running this script to apply the changes to /etc/profile
 
-# Install Golang
+# Install dependencies
 sudo apt-get update
 sudo apt-get -y upgrade
+yes | sudo apt install \
+    make \
+    gcc
 
+# Install Golang
 wget https://dl.google.com/go/go1.16.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.16.linux-amd64.tar.gz
-
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
